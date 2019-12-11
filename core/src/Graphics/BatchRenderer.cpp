9870a2e51bdd7a0fdec8987876421821819eacbd
durswd <swda.durl@gmail.com>
2019-12-12 00:50:29 +0900

    Implementing new renderer

---
+++ core/src/Graphics/BatchRenderer.cpp
@@ -0,0 +1,73 @@
+#include "BatchRenderer.h"
+#include "../Graphics/Graphics.h"
+
+namespace altseed {
+
+BatchRenderer::BatchRenderer(std::shared_ptr<Graphics> graphics) {
+    auto graphicsLLGI = graphics->GetGraphicsLLGI();
+    rawVertexBuffer_.reserve(VertexBufferMax);
+    vertexBuffer_ = LLGI::CreateSharedPtr(graphicsLLGI->CreateVertexBuffer(sizeof(BatchVertex) * VertexBufferMax));
+    indexBuffer_ = LLGI::CreateSharedPtr(graphicsLLGI->CreateIndexBuffer(4, IndexBufferMax));
+}
+
+void BatchRenderer::Draw(
+        const BatchVertex* vb, const int32_t* ib, int32_t vbCount, int32_t ibCount, const std::shared_ptr<Material>& material) {
+    if (batches_.size() == 0 || batches_.back().material != material) {
+        Batch batch;
+        batch.material = material;
+        batch.VertexCount = 0;
+        batch.IndexCount = 0;
+        batch.VertexOffset = rawVertexBuffer_.size();
+        batch.IndexOffset = rawIndexBuffer_.size();
+        batches_.emplace_back(batch);
+    }
+
+    auto& b = batches_.back();
+
+    auto current = rawVertexBuffer_.size();
+
+    for (int32_t i = 0; i < vbCount; i++) {
+        rawVertexBuffer_.emplace_back(vb[i]);
+    }
+
+    for (int32_t i = 0; i < ibCount; i++) {
+        rawIndexBuffer_.emplace_back(ib[i] + current);
+    }
+
+    b.VertexCount += vbCount;
+    b.IndexCount += ibCount;
+}
+
+void BatchRenderer::Render(CommandList* commandList) {
+    if (batches_.size() == 0) return;
+
+    if (VertexBufferMax < vbOffset_ + rawVertexBuffer_.size()) {
+        vbOffset_ = 0;
+    }
+
+    if (IndexBufferMax < ibOffset_ + rawIndexBuffer_.size()) {
+        vbOffset_ = 0;
+    }
+
+    // TODO need to cause warning
+    if (VertexBufferMax < rawVertexBuffer_.size()) return;
+
+    // TODO need to cause warning
+    if (IndexBufferMax < rawIndexBuffer_.size()) return;
+
+    auto pvb = static_cast<BatchVertex*>(vertexBuffer_->Lock()) + vbOffset_;
+    auto pib = static_cast<int32_t*>(indexBuffer_->Lock()) + ibOffset_;
+
+    vertexBuffer_->Unlock();
+    indexBuffer_->Unlock();
+
+
+}
+
+void BatchRenderer::ResetCache() {
+    batches_.resize(0);
+    rawVertexBuffer_.resize(0);
+    rawIndexBuffer_.resize(0);
+}
+
+}  // namespace altseed
--- core/src/Graphics/Material.cpp
+++ core/src/Graphics/Material.cpp
@@ -1,5 +1,40 @@
 ﻿#include "Material.h"
 
-// hogehoge
 #include <glslang/Public/ShaderLang.h>
 #include <spirv_cross/spirv.hpp>
+
+namespace altseed {
+
+std::shared_ptr<LLGI::PipelineState> Material::GetPipelineState(LLGI::RenderPass* renderPass) {
+    auto g = Graphics::GetInstance()->GetGraphicsLLGI();
+
+    auto renderPassPipelineState = LLGI::CreateSharedPtr(g->CreateRenderPassPipelineState(renderPass));
+    auto it = pipelineStates_.find(renderPassPipelineState);
+    if (it != pipelineStates_.end()) {
+        return it->second;
+    }
+
+    auto piplineState = LLGI::CreateSharedPtr(g->CreatePiplineState());
+    piplineState->SetShader(LLGI::ShaderStageType::Vertex, vertexShader_->Get());
+    piplineState->SetShader(LLGI::ShaderStageType::Pixel, shader_->Get());
+    piplineState->Culling = LLGI::CullingMode::DoubleSide;
+    piplineState->SetRenderPassPipelineState(renderPassPipelineState.get());
+    // TODO
+    /*
+    pip->VertexLayouts[0] = LLGI::VertexLayoutFormat::R32G32B32_FLOAT;
+    pip->VertexLayouts[1] = LLGI::VertexLayoutFormat::R32G32_FLOAT;
+    pip->VertexLayouts[2] = LLGI::VertexLayoutFormat::R8G8B8A8_UNORM;
+    pip->VertexLayoutNames[0] = "POSITION";
+    pip->VertexLayoutNames[1] = "UV";
+    pip->VertexLayoutNames[2] = "COLOR";
+    pip->VertexLayoutCount = 3;
+    */
+
+    piplineState->Compile();
+
+    pipelineStates_[renderPassPipelineState] = piplineState;
+
+    return piplineState;
+}
+
+}  // namespace altseed
