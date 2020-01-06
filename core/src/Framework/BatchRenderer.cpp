66db6291beddb0e0bc0a9407aa46e20034aede0e
durswd <swda.durl@gmail.com>
2020-01-06 09:05:58 +0900

    wip

--- core/src/Framework/BatchRenderer.cpp
+++ core/src/Framework/BatchRenderer.cpp
@@ -1,2 +1,63 @@
 #include "BatchRenderer.h"
+#include "../Graphics/Graphics.h"
 
+namespace altseed {
+
+BatchRenderer::BatchRenderer(std::shared_ptr<Graphics> graphics) {
+    auto graphicsLLGI = graphics->GetGraphicsLLGI();
+    rawVertexBuffer_.reserve(VertexBufferMax);
+    vertexBuffer_ = LLGI::CreateSharedPtr(graphicsLLGI->CreateVertexBuffer(sizeof(BatchVertex) * VertexBufferMax));
+    indexBuffer_ = LLGI::CreateSharedPtr(graphicsLLGI->CreateIndexBuffer(4, IndexBufferMax));
+}
+
+void BatchRenderer::Draw(
+        const BatchVertex* vb, const int32_t* ib, int32_t vbCount, int32_t ibCount, const std::shared_ptr<Material>& material) {
+    if (batches_.size() == 0 || batches_.back().material != material) {
+        Batch batch;
+        batch.material = material;
+        batch.VertexCount = 0;
+        batch.IndexCount = 0;
+        batch.VertexOffset = rawVertexBuffer_.size();
+        batch.IndexOffset = rawIndexBuffer_.size();
+        batches_.emplace_back(batch);
+    }
+
+    auto& b = batches_.back();
+
+    auto current = rawVertexBuffer_.size();
+
+    for (int32_t i = 0; i < vbCount; i++) {
+        rawVertexBuffer_.emplace_back(vb[i]);
+    }
+
+    for (int32_t i = 0; i < ibCount; i++) {
+        rawIndexBuffer_.emplace_back(ib[i] + current);
+    }
+
+    b.VertexCount += vbCount;
+    b.IndexCount += ibCount;
+}
+
+void BatchRenderer::Render(std::shared_ptr<CommandList> commandList) {
+    if (batches_.size() == 0) return;
+
+    if (VertexBufferMax < vbOffset_ + rawVertexBuffer_.size()) {
+        vbOffset_ = 0;
+    }
+
+    if (IndexBufferMax < ibOffset_ + rawIndexBuffer_.size()) {
+        vbOffset_ = 0;
+    }
+
+    if (VertexBufferMax < rawVertexBuffer_.size()) return;
+
+    if (IndexBufferMax < rawIndexBuffer_.size()) return;
+}
+
+void BatchRenderer::ResetCache() {
+    batches_.resize(0);
+    rawVertexBuffer_.resize(0);
+    rawIndexBuffer_.resize(0);
+}
+
+}  // namespace altseed
