bbfd0986e1ff0abeb0c1151f53b274546969676a
durswd <swda.durl@gmail.com>
2020-01-21 13:13:16 +0900

    Implement buildin shader

--- core/src/Graphics/Material.cpp
+++ core/src/Graphics/Material.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Material.h"
+#include "BuildinShader.h"
 
 #include <glslang/Public/ShaderLang.h>
 #include <spirv_cross/spirv.hpp>
@@ -14,21 +15,30 @@ std::shared_ptr<LLGI::PipelineState> Material::GetPipelineState(LLGI::RenderPass
         return it->second;
     }
 
+	if (vertexShader_ == nullptr)
+	{
+        vertexShader_ = Graphics::GetInstance()->GetBuildinShader()->Create(BuildinShaderType::SpriteUnlitVS);
+	}
+
     auto piplineState = LLGI::CreateSharedPtr(g->CreatePiplineState());
     piplineState->SetShader(LLGI::ShaderStageType::Vertex, vertexShader_->Get());
     piplineState->SetShader(LLGI::ShaderStageType::Pixel, shader_->Get());
     piplineState->Culling = LLGI::CullingMode::DoubleSide;
     piplineState->SetRenderPassPipelineState(renderPassPipelineState.get());
-    // TODO
-    /*
-    pip->VertexLayouts[0] = LLGI::VertexLayoutFormat::R32G32B32_FLOAT;
-    pip->VertexLayouts[1] = LLGI::VertexLayoutFormat::R32G32_FLOAT;
-    pip->VertexLayouts[2] = LLGI::VertexLayoutFormat::R8G8B8A8_UNORM;
-    pip->VertexLayoutNames[0] = "POSITION";
-    pip->VertexLayoutNames[1] = "UV";
-    pip->VertexLayoutNames[2] = "COLOR";
-    pip->VertexLayoutCount = 3;
-    */
+
+    piplineState->VertexLayouts[0] = LLGI::VertexLayoutFormat::R32G32B32_FLOAT;
+    piplineState->VertexLayouts[1] = LLGI::VertexLayoutFormat::R8G8B8A8_UNORM;
+    piplineState->VertexLayouts[2] = LLGI::VertexLayoutFormat::R32G32_FLOAT;
+    piplineState->VertexLayouts[3] = LLGI::VertexLayoutFormat::R32G32_FLOAT;
+    piplineState->VertexLayoutNames[0] = "POSITION";
+    piplineState->VertexLayoutNames[1] = "COLOR";
+    piplineState->VertexLayoutNames[2] = "UV";
+    piplineState->VertexLayoutNames[3] = "UV";
+    piplineState->VertexLayoutSemantics[0] = 0;
+    piplineState->VertexLayoutSemantics[1] = 0;
+    piplineState->VertexLayoutSemantics[2] = 0;
+    piplineState->VertexLayoutSemantics[3] = 1;
+    piplineState->VertexLayoutCount = 4;
 
     piplineState->Compile();
 
